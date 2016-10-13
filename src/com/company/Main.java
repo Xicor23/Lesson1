2d636875c1629433e985b5c3e2ec175f38c9ccae
Ye, Will <statisticslol1234@gmail.com>
2016-10-13 18:06:44 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World");
+        System.out.println("What is your name?");
+        Scanner input = new Scanner(System.in);
+        String c = input.next();
+        perfectRandom newname = new perfectRandom(c);
+        Random rand = new Random();
+        int number = rand.nextInt(100);
+        newname.getNumber(number);
+
     }
 }
